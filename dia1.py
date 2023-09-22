b4fd571a20288532a7306fed017eb20b69fa25cf
LoretoLopez <145747001+LoretoLopez@users.noreply.github.com>
2023-09-22 11:10:18 +0200

    Add files via upload

---
+++ dia1.py
@@ -0,0 +1,41 @@
+#Que bonito es python
+print("Hola mundo")
+print("me llamo Loreto")
+print(loreto)
+#nuestra primera variable
+x=5
+print("print(5):")
+print(5)  #imprime 5
+print("print(5+5):")
+print(5+5) #imprime 10
+print("print(x):")
+print(x) #imprime 5
+
+
+#creamos una variable con texto- string
+nombre=  "celia"
+numero= 8
+print(nombre)
+print("sumo nombre+numero:")
+print("celia"+"8")
+print('(nombre+(str)numero)):')
+print(nombre+str(numero))
+print("Cucu"*10)
+print("escribo diez veces cucu")
+print()
+print("\n\\Hacemos \"operaciones \"\t matematicas")
+#barra con n salto de linea
+#barra con t tabulacion
+#barra con barra
+#anular funcionalidad comillas con barra
+#si quieres poner una barra tienes que poner dos.
+
+num1=2
+num2=8
+suma =num1+num2
+resta=num1-num2
+multp=num1*num2
+division=num1/num2
+
+print("suma=" +str(suma)+ "  | Resta = " str(resta) + "Multiplicacion= " str(multp) +
+
